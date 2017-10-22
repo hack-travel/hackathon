f835c1c8d491ab0fff4cb60370f21c0e1219de52
Craig Gordon <cyghfer@gmail.com>
2017-10-22 14:28:30 -0400

    Set up essential folders/files for the Prep section

--- client/components/prep/essentials/EssentialsHome.jsx
+++ client/components/prep/essentials/EssentialsHome.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import PrepHome from './PrepHome.jsx';
-import PackingHome from './packing/PackingHome.jsx';
-import BudgetHome from './budget/BudgetHome.jsx';
-import EssentialsHome from './essentials/EssentialsHome.jsx';
-import TranslationHome from './translation/TranslationHome.jsx';
 
 // React-Redux connect() boilerplate
 // NOTE: you may have to modify the filepath for ActionCreators
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as ActionCreators from '../../actions';
+import * as ActionCreators from '../../../actions';
 
-class PrepIndex extends React.Component {
+class EssentialsHome extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div className='prepIndex'>
-        <h2>Prepare for Your Trip</h2>
-        <Switch>
-          <Route exact path='/prep' component={PrepHome} />
-          <Route path='/prep/packing' component={PackingHome} />
-          <Route path='/prep/budget' component={BudgetHome} />
-          <Route path='/prep/essentials' component={EssentialsHome} />
-          <Route path='/prep/translation' component={TranslationHome} />
-        </Switch>
+      <div className='essentialsHome'>
+        <p>Essentials Home</p>
       </div>
     );
   }
@@ -51,4 +39,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(PrepIndex));
+)(EssentialsHome));
