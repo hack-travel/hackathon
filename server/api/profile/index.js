68ebc8e77fc8b040decc539044c1ab6f15a010d1
Alexander Lukens <alexander.lukens1@gmail.com>
2017-10-25 13:59:39 -0400

    Finish login process

--- server/api/profile/index.js
+++ server/api/profile/index.js
@@ -2,23 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../../db/index');
 
-router.post('/login', (req, res) => {
-  const fbProfile = req.body.profile
-  console.log(fbProfile)
-  db.User.create({
-    username: fbProfile.name,
-    password: '',
-    email: fbProfile.email,
-    fbId: parseInt(fbProfile.id),
-    sex: fbProfile.gender,
-    age: 10,
-  })
-  .then((data) => {
-      res.send(data);
-  })
-  .catch((e) => {
+router.post('/login', async (req, res) => {
+  try {
+    const fbProfile = req.body.profile
+    console.log(fbProfile)
+    let checkForUser = await db.User.find({
+      where: {
+        fbId: parseInt(fbProfile.id)
+      }
+    })
+    if (checkForUser) {
+      res.send('User Exists')
+      return;
+    }
+    let newUser = await db.User.create({
+      username: fbProfile.name,
+      password: '',
+      email: fbProfile.email,
+      fbId: parseInt(fbProfile.id),
+      sex: fbProfile.gender,
+      age: 10
+    })
+    res.send(newUser)
+  } catch (e) {
     console.error(e);
-  })
+  }
 });
 
 
