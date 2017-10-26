3dd487732e612d3a36e3b4c610d5a5176c7c3638
kinyusui <kaixngchen@gmail.com>
2017-10-26 13:48:53 -0400

    we

--- client/components/prep/budget/CreateBudget.jsx
+++ client/components/prep/budget/CreateBudget.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 class CreateBudget extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      Flight: '',
-      Hotel: '',
-      Food: '',
-      PublicTransport: '',
-      Souvenirs: '',
-      EmergencyFund: ''
+      Flight: 0,
+      Hotel: 0,
+      Food: 0,
+      PublicTransport: 0,
+      Souvenirs: 0,
+      EmergencyFund: 0
     }
     this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit (e) {
+    e.preventDefault();
+    let s = this.state;
+    axios.post('/prep/budget', {
+      Flight: s.Flight,
+      Hotel: s.Hotel,
+      Food: s.Food,
+      PublicTransport: s.PublicTransport,
+      Souvenirs: s.Souvenirs,
+      EmergencyFund: s.EmergencyFund
+    })
   }
   handleChange (e) {
     this.setState({
@@ -29,6 +42,7 @@ class CreateBudget extends React.Component {
         Souvenirs:<input value={this.state.Souvenirs} onChange={this.handleChange} data-type='Souvenirs'/><br/>
         EmergencyFund:<input value={this.state.EmergencyFund} onChange={this.handleChange} data-type='EmergencyFund'/><br/>
       </form>
+      <button onClick={this.handleSubmit}>Make Budget Plan</button>
     </div>
   }
 }
--- server/api/prep/index.js
+++ server/api/prep/index.js
@@ -4,8 +4,20 @@ const db = require('../../../db');
 
 const sequelize = db.sequelize;
 
-router.get('/budget', (req, res) => {
-  
+router.post('/budget', async (req, res) => {
+  try {
+    const budget = req.body.budget;
+    let result = db.Budget.create({
+      Flight: budget.flight,
+      Hotel: budget.Hotel,
+      Food: budget.food,
+      PublicTransport: budget.PublicTransport,
+      Souvenirs: budget.Souvenirs,
+      EmergencyFund: budget.EmergencyFund
+    })
+  } catch (e) {
+    console.error('prep index.js ln 20', e);
+  }
 })
 
 module.exports = router;
